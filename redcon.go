ce1fdde84526177c1c0d4a6b60a321e17f098801
zhoufeng1989 <zhoufengloop@gmail.com>
2022-12-06 13:54:58 +0800

    Remove debug logs.

--- redcon.go
+++ redcon.go
@@ -395,7 +395,6 @@ func serve(s *Server) error {
 	for {
 		lnconn, err := s.ln.Accept()
 		if err != nil {
-			s.LogMessage(fmt.Sprintf("accept error %s", err))
 			if s.AcceptError != nil {
 				s.AcceptError(err)
 			}
@@ -403,7 +402,6 @@ func serve(s *Server) error {
 			done := s.done
 			s.mu.Unlock()
 			if done {
-				s.LogMessage("server is done")
 				return nil
 			}
 			continue
@@ -414,14 +412,11 @@ func serve(s *Server) error {
 			wr:   NewWriter(lnconn),
 			rd:   NewReader(lnconn),
 		}
-		s.LogMessage(fmt.Sprintf("accept connection %+v", c))
 		s.mu.Lock()
 		c.idleClose = s.idleClose
 		s.conns[c] = true
 		s.mu.Unlock()
-		s.LogMessage(fmt.Sprintf("add connection to server conns list %+v", c))
 		if s.accept != nil && !s.accept(c) {
-			s.LogMessage(fmt.Sprintf("accept callback error %+v", c))
 			s.mu.Lock()
 			delete(s.conns, c)
 			s.mu.Unlock()
@@ -436,15 +431,10 @@ func serve(s *Server) error {
 func handle(s *Server, c *conn) {
 	var err error
 	defer func() {
-		if err != nil && err != errDetached && err != io.EOF {
-			message := fmt.Sprintf("handle connection error %+v %s", c, err)
-			s.LogMessage(message)
-		}
 		if err != errDetached {
-			s.LogMessage(fmt.Sprintf("close connection %+v %s", c, err))
 			_, closeErr := c.Close(true)
 			if closeErr != nil {
-				message := fmt.Sprintf("close connection error in handle defer %+v %s", c, err)
+				message := fmt.Sprintf("close connection error %+v %s", c, err)
 				s.LogMessage(message)
 			}
 		}
@@ -469,8 +459,6 @@ func handle(s *Server, c *conn) {
 			}
 			cmds, err := c.rd.readCommands(nil)
 			if err != nil {
-				message := fmt.Sprintf("connection reads commands error %+v %s", c, err)
-				s.LogMessage(message)
 				if err, ok := err.(*errProtocol); ok {
 					// All protocol errors should attempt a response to
 					// the client. Ignore write errors.
@@ -479,7 +467,6 @@ func handle(s *Server, c *conn) {
 				}
 				return err
 			}
-			s.LogMessage(fmt.Sprintf("handle commands %+v", cmds))
 			if len(cmds) > 0 {
 				s.handler(c, cmds)
 			}
@@ -491,15 +478,11 @@ func handle(s *Server, c *conn) {
 				return nil
 			}
 			if err := c.wr.Flush(); err != nil {
-				message := fmt.Sprintf("connection writes commands error %+v %s", c, err)
-				s.LogMessage(message)
 				return err
 			}
 			if s.IsServerClosing() && !c.InTx() {
 				return nil
 			}
-			message := fmt.Sprintf("continue connection %+v serverStatus %t", c, s.IsServerClosing())
-			s.LogMessage(message)
 		}
 	}()
 }
